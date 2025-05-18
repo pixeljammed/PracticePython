668f21294de1f9141394739629dcb7b13a79ffeb
mtek <milo@milotek.dev>
2025-05-18 12:20:06 +0100

    Add tiktok print

---
+++ TikTok Print.py
@@ -0,0 +1,18 @@
+### Milo Tek - 15/05/2025 - Tiktok Print ###
+## Based off a TikTok I saw ages ago, which showed someone making their own print function in a cool way!
+import string, time
+
+def tiktokprint(text):
+    text = text.upper()
+    letters = " " + string.ascii_uppercase 
+    output = [""]*len(text)
+    for x in range(0, len(text)):   
+        for letter in letters:
+            time.sleep(0.05)
+            output[x] = letter
+            print(*output)
+            if text[x] == output[x]:
+                break
+
+text = "I eat and devour babies"
+tiktokprint(text)
